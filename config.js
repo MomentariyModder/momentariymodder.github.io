019f60150ed97e9e2e5e11dccc37e6fcfce8dbb4
MomentariyModder <82979292+MomentariyModder@users.noreply.github.com>
2024-10-24 10:59:40 +0300

    7.0.0
    наконец-то

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 /*!
- * MomentariyModder Website 6.0 by MomentariyModder
+ * MomentariyModder Website 7.0.0 by MomentariyModder
  * The source code is available on GitHub!
  */
 
 var server_name = "MomentariyModder";
-var server_ip = "Website 6.7.0";
+var server_ip = "Website 7.0.0";
 var server_port = 25565;
 var ip_copied = "IP copied!";
 
